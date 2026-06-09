713aaacdc744e9e1d2a7215fa7f82fc2469427fb
Hossamomar2nd <hossamomar2nd@gmail.com>
2026-06-09 23:58:44 +0300

    Update Admin code format

--- API/src/modules/admin/adminSchema.js
+++ API/src/modules/admin/adminSchema.js
@@ -1,9 +1,10 @@
-
 import Joi from "joi";
 
 export const updateInfoSchema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    image_url: Joi.string()
+  name: Joi.string(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  image_url: Joi.string(),
 });
